96fb685fde72557a3e6c98a385add2e93e53e104
Nattapon Pondongnok <nainatjab999@gmail.com>
2021-03-19 23:41:22 +0700

    check if 80

--- config/config.ts
+++ config/config.ts
@@ -1,6 +1,11 @@
 import "https://deno.land/x/dotenv/load.ts";
 
-export const HOSTNAME = `${Deno.env.get('HOST')}:${Deno.env.get('PORT')}` || "http://localhost";
+let host = Deno.env.get('HOST')
+let port = Deno.env.get('PORT')
+
+let hostName = port == "80" ? host : `${host}:${port}`;
+
+export const HOSTNAME = `${hostName}` || "http://localhost";
 export const DATABASE_NAME = "project";
 export const TABLE = {
     LINK: "link"
