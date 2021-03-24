4aaaaf8a2e1541b7a6e4eb1465b93919044d652f
Nattapon Pondongnok <nainatjab999@gmail.com>
2021-03-25 00:30:12 +0700

    insert stat if not any link

--- controller/urlShortener.ts
+++ controller/urlShortener.ts
@@ -4,95 +4,111 @@ import Link from "../interface/Link.ts";
 import Counter from "../interface/Counter.ts";
 
 export default {
-    hello: async ({ response }: { params: {}; response: any },) => {
-        response.status = 200;
-        response.body = {
-            message: `Hello from: ${APP_NAME}`,
-        };
-    },
-    getAll: async ({ params, response }: { params: {}; response: any },) => {
-        try{
-            const result = await linkRepository.getAll();
-            response.status = 200;
-            response.body = result;
-        } catch (error) {
-            response.status = 400;
-            response.body = {
-                message: `Error: ${error}`,
-            };
-        }
-    },
-    getByShort: async ({ params, response }: { params: { short: string }; response: any },) => {
-        try {
-            const short = params.short
-            const link: Link = await linkRepository.getByShort({ short })
-            if (!link) {
-                response.status = 404;
-                response.body = {
-                    message: `Link not found`,
-                };
-                return;
-            }
-            const updateResult = await linkRepository.updateStatByShort({ short })
-            if (updateResult > 0) {
-                response.status = 302;
-                response.headers.set("Location", link.full)
-                return;
-            }
-        } catch (error) {
-            response.status = 400;
-            response.body = {
-                message: `Error: ${error}`,
-            };
-        }
-    },
-    getStatByShort: async ({ params, response }: { params: { short: string }; response: any },) => {
-        try {
-            const link: Counter = await linkRepository.getStatByShort({
-                short: params.short,
-            });
-            if (!link) {
-                response.status = 404;
-                response.body = {
-                    message: `Link not found`,
-                };
-                return;
-            }
-            response.status = 200;
-            response.body = {
-                visit: link.count,
-            };
-            return;
-        } catch (error) {
-            response.status = 400;
-            response.body = {
-                message: `Error: ${error}`,
-            };
-        }
-    },
-    create: async ({ request, response }: { request: any; response: any }) => {
-        const body = await request.body().value;
-        if (!body.url) {
-            response.status = 400;
-            response.body = {
-                message: "Invalid body",
-            };
-            return;
-        }
-        try {
-            const link = await linkRepository.create({
-                full: body.url
-            })
-            response.status = 200;
-            response.body = {
-                link: link.short,
-            };
-            return;
-        } catch (error) {
-            response.status = 400;
-            response.body = {
-                message: `Error: ${error}`,
-            };
-        }
+  hello: async ({ response }: { params: {}; response: any }) => {
+    response.status = 200;
+    response.body = {
+      message: `Hello from: ${APP_NAME}`,
+    };
+  },
+  getAll: async ({ params, response }: { params: {}; response: any }) => {
+    try {
+      const result = await linkRepository.getAll();
+      response.status = 200;
+      response.body = result;
+    } catch (error) {
+      response.status = 400;
+      response.body = {
+        message: `Error: ${error}`,
+      };
     }
-}
+  },
+  getByShort: async ({
+    params,
+    response,
+  }: {
+    params: { short: string };
+    response: any;
+  }) => {
+    try {
+      const short = params.short;
+      const link: Link = await linkRepository.getByShort({ short });
+      if (!link) {
+        response.status = 404;
+        response.body = {
+          message: `Link not found`,
+        };
+        return;
+      }
+      const updateResult = await linkRepository.updateStatByShort({ short });
+      if (updateResult > 0) {
+        response.status = 302;
+        response.headers.set("Location", link.full);
+        return;
+      }
+    } catch (error) {
+      response.status = 400;
+      response.body = {
+        message: `Error: ${error}`,
+      };
+    }
+  },
+  getStatByShort: async ({
+    params,
+    response,
+  }: {
+    params: { short: string };
+    response: any;
+  }) => {
+    try {
+      const link: Link = await linkRepository.getByShort({
+        short: params.short,
+      });
+      if (!link) {
+        response.status = 404;
+        response.body = {
+          message: `Link not found`,
+        };
+        return;
+      }
+      const stat: Counter = await linkRepository.getStatByShort({
+        short: params.short,
+      });
+      console.log(`Stat By Short ${stat.count}`)
+      response.status = 200;
+      response.body = {
+        visit: stat.count,
+      };
+      return;
+    } catch (error) {
+      response.status = 400;
+      response.body = {
+        message: `Error: ${error}`,
+      };
+    }
+  },
+  create: async ({ request, response }: { request: any; response: any }) => {
+    const body = await request.body().value;
+    if (!body.url) {
+      response.status = 400;
+      response.body = {
+        message: "Invalid body",
+      };
+      return;
+    }
+    try {
+      const link = await linkRepository.create({
+        full: body.url,
+      });
+      response.status = 200;
+      response.body = {
+        link: link.short,
+      };
+      return;
+    } catch (error) {
+      response.status = 400;
+      response.body = {
+        message: `Error: ${error}`,
+      };
+    }
+  },
+};
--- repository/link.ts
+++ repository/link.ts
@@ -1,74 +1,94 @@
 import client from "../config/db.ts";
 import Link from "../interface/Link.ts";
-import {HOSTNAME, TABLE} from "../config/config.ts";
+import { HOSTNAME, TABLE } from "../config/config.ts";
 import { nanoid } from "https://deno.land/x/nanoid/mod.ts";
 
 export default {
-    getAll: async () => {
-        return await client.query(`SELECT * FROM ${TABLE.LINK}`);
-    },
-    getByShort: async ( { short }: Link) => {
-        const result = await client.query(
-            `SELECT full FROM ${TABLE.LINK} WHERE short = ? LIMIT 1`,
-            [short],
-        );
-        return result[0]
-    },
-    getStatByShort: async ( { short }: Link) => {
-        const result = await client.query(
-            `SELECT count FROM ${TABLE.COUNTER} WHERE id = ? LIMIT 1`,
-            [short],
-        );
-        return result[0]
-    },
-    create: async ({ full }: Link) => {
-        const resultFull = await client.query(
-            `SELECT short, full FROM ${TABLE.LINK} WHERE full = ? LIMIT 1`,
-            [full]
-        );
+  getAll: async () => {
+    return await client.query(`SELECT * FROM ${TABLE.LINK}`);
+  },
+  getByShort: async ({ short }: Link) => {
+    const result = await client.query(
+      `SELECT full FROM ${TABLE.LINK} WHERE short = ? LIMIT 1`,
+      [short]
+    );
+    return result[0];
+  },
+  getStatByShort: async ({ short }: Link) => {
+    const resultExistShortCount = await client.query(
+      `SELECT count FROM ${TABLE.COUNTER} WHERE id = ? LIMIT 1`,
+      [short]
+    );
 
-        // Check if have exist full url
-        let existsURL = resultFull[0]
-        if (existsURL) {
-            return { short: `${HOSTNAME}/l/${existsURL.short}` };
-        }
-        // It not have any full url
-        let genId = nanoid(6);
-        const result = await client.query(
-            `INSERT INTO ${TABLE.LINK} (short, full) VALUES (?, ?)`,
-            [genId, full]
-        );
-        if (result.affectedRows == 0) {
-            return Promise.reject("Cannot Create Link");
-        }
-        return { short: `${HOSTNAME}/l/${genId}` };
-    },
-    updateStatByShort: async ({ short }: Link) => {
-        const resultShort = await client.query(
-            `SELECT id FROM ${TABLE.COUNTER} WHERE id = ? LIMIT 1`,
-            [short]
-        );
-
-        // Check if have exist short url
-        let existsURL = resultShort[0]
-        if (existsURL) {
-            const result = await client.query(
-                `UPDATE ${TABLE.COUNTER} SET count = count + 1 WHERE id = ?`,
-                [short]
-            );
-            if (result.affectedRows == 0) {
-                return Promise.reject("Cannot Update Count Link")
-            }
-            return result.affectedRows;
-        }
-        // It not have any short url
-        const result = await client.query(
-            `INSERT INTO ${TABLE.COUNTER} (id, count) VALUES (?, ?)`,
-            [short, 1]
-        );
-        if (result.affectedRows == 0) {
-            return Promise.reject("Cannot Create Link");
-        }
-        return result.affectedRows;
+    // Check if have exist count link
+    let existsURL = resultExistShortCount[0];
+    if (existsURL) {
+      return existsURL;
     }
-}
+
+    // It not have any count link
+    const result = await client.query(
+      `INSERT INTO ${TABLE.COUNTER} (id, count) VALUES (?, ?)`,
+      [short, 0]
+    );
+    if (result.affectedRows == 0) {
+      return Promise.reject("Cannot Create Link");
+    }
+
+    const resultShortCount = await client.query(
+      `SELECT count FROM ${TABLE.COUNTER} WHERE id = ? LIMIT 1`,
+      [short]
+    );
+    return resultShortCount[0];
+  },
+  create: async ({ full }: Link) => {
+    const resultFull = await client.query(
+      `SELECT short, full FROM ${TABLE.LINK} WHERE full = ? LIMIT 1`,
+      [full]
+    );
+
+    // Check if have exist full url
+    let existsURL = resultFull[0];
+    if (existsURL) {
+      return { short: `${HOSTNAME}/l/${existsURL.short}` };
+    }
+    // It not have any full url
+    let genId = nanoid(6);
+    const result = await client.query(
+      `INSERT INTO ${TABLE.LINK} (short, full) VALUES (?, ?)`,
+      [genId, full]
+    );
+    if (result.affectedRows == 0) {
+      return Promise.reject("Cannot Create Link");
+    }
+    return { short: `${HOSTNAME}/l/${genId}` };
+  },
+  updateStatByShort: async ({ short }: Link) => {
+    const resultShort = await client.query(
+      `SELECT id FROM ${TABLE.COUNTER} WHERE id = ? LIMIT 1`,
+      [short]
+    );
+
+    // Check if have exist short url
+    let existsURL = resultShort[0];
+    if (existsURL) {
+      const result = await client.query(
+        `UPDATE ${TABLE.COUNTER} SET count = count + 1 WHERE id = ?`,
+        [short]
+      );
+      if (result.affectedRows == 0) {
+        return Promise.reject("Cannot Update Count Link");
+      }
+      return result.affectedRows;
+    }
+    // It not have any short url
+    const result = await client.query(
+      `INSERT INTO ${TABLE.COUNTER} (id, count) VALUES (?, ?)`,
+      [short, 1]
+    );
+    if (result.affectedRows == 0) {
+      return Promise.reject("Cannot Create Counter Link");
+    }
+    return result.affectedRows;
+  },
+};
