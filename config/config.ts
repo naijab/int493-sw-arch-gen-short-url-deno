bb996a90e612907b9c7e373df06c5302e831e1a8
Nattapon Pondongnok <nainatjab999@gmail.com>
2021-04-17 01:11:50 +0700

    update version

--- config/config.ts
+++ config/config.ts
@@ -6,7 +6,7 @@ let port = Deno.env.get("PORT");
 let hostName = port == "80" ? host : `${host}:${port}`;
 
 export const APP_NAME = Deno.env.get("APP_NAME");
-export const APP_VERSION = "v.1.4";
+export const APP_VERSION = "v.1.4 (redis single mode)";
 export const HOSTNAME = `${hostName}` || "http://localhost";
 
 export const DATABASE_NAME = "project";
